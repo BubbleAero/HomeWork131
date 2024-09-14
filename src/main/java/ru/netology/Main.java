1237610c000a3437c0e35d3a1762eb79cd07c633
NetoTest <vikavasilenko2013@gmail.com>
2024-09-14 13:55:10 +0300

    Tests

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,17 @@
 package ru.netology;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ShopRepository repo = new ShopRepository();
+
+        try {
+            repo.removeById(-100);
+            //System.out.println("Hello!");
+        } catch (NegativeArraySizeException e) {
+            System.out.println("Ошибка");
+        }
+
+
     }
 }
---
+++ src/test/java/ShopRepositoryTest.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.AlreadyExistsException;
+import ru.netology.NotFoundException;
+import ru.netology.Product;
+import ru.netology.ShopRepository;
+
+public class ShopRepositoryTest {
+
+    @Test
+    public void shouldRemoveProductIfExists() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(1, "Продукт 1", 100);
+        Product product2 = new Product(2, "Продукт 2", 200);
+        repo.add(product1);
+        repo.add(product2);
+
+        repo.removeById(1);
+
+        Product[] expected = { product2 };
+        Product[] actual = repo.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowExceptionIfNotFound() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(1, "Продукт 1", 100);
+        Product product2 = new Product(2, "Продукт 2", 200);
+        repo.add(product1);
+        repo.add(product2);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.removeById(3);
+        });
+    }
+
+
+        @Test
+        public void shouldAddProductSuccessfully() {
+            ShopRepository repo = new ShopRepository();
+            Product product1 = new Product(1, "Продукт 1", 100);
+
+            repo.add(product1);
+
+            Product[] expected = {product1};
+            Product[] actual = repo.findAll();
+
+            Assertions.assertArrayEquals(expected, actual);
+        }
+
+        @Test
+        public void shouldThrowAlreadyExistsException() {
+            ShopRepository repo = new ShopRepository();
+            Product product1 = new Product(1, "Продукт 1", 100);
+            Product product2 = new Product(1, "Продукт 2", 200);
+            repo.add(product1);
+
+            Assertions.assertThrows(AlreadyExistsException.class, () -> {
+                repo.add(product2);
+            });
+        }
+    }
+
